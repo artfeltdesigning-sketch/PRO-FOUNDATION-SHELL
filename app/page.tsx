2cab9059743720776fd6e8fcd50a344bcbc22a94
artfeltdesigning-sketch <artfeltdesigning@gmail.com>
2026-05-24 16:54:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { useState } from "react";
+import Landing from "../components/landing";
 import AppShell from "../components/app-shell";
 
 export default function Home() {
-  return <AppShell />;
+  const [entered, setEntered] =
+    useState(false);
+
+  return entered ? (
+    <AppShell />
+  ) : (
+    <Landing
+      onEnter={() =>
+        setEntered(true)
+      }
+    />
+  );
 }
