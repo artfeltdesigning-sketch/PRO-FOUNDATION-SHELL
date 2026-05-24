80362cdd149b6aa1561987db9c613d0aa70e7b95
artfeltdesigning-sketch <artfeltdesigning@gmail.com>
2026-05-24 14:33:15 +0530

    Update app-shell.tsx

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -1,21 +1,15 @@
 "use client";
 
 import Sidebar from "./Sidebar";
-import Topbar from "./topbar";
 import PromptWorkspace from "./PromptWorkspace";
-import ResultConsole from "./result-console";
+import ResultPanel from "./ResultPanel";
 
 export default function AppShell() {
   return (
     <main className="app-shell">
       <Sidebar />
-
-      <div className="main-stack">
-        <Topbar />
-        <PromptWorkspace />
-      </div>
-
-      <ResultConsole />
+      <PromptWorkspace />
+      <ResultPanel />
     </main>
   );
 }
