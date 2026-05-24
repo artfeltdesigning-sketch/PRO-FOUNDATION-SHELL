8c2d3a6a0b85a6222a7d8f4564dd0ecfe2808468
artfeltdesigning-sketch <artfeltdesigning@gmail.com>
2026-05-24 15:04:09 +0530

    Update app-shell.tsx

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -1,17 +1,23 @@
 "use client";
 
+import { useState } from "react";
 import Sidebar from "./sidebar";
 import PromptWorkspace from "./prompt-workspace";
 import ResultPanel from "./result-panel";
 
 export default function AppShell() {
+  const [output, setOutput] =
+    useState("");
+
   return (
     <main className="app-shell">
       <Sidebar />
 
-      <PromptWorkspace />
+      <PromptWorkspace
+        onGenerate={setOutput}
+      />
 
-      <ResultPanel />
+      <ResultPanel output={output} />
     </main>
   );
 }
